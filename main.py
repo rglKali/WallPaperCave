162bb27a218b4afe8c73284bd0869d45fb7eeffd
rglKali <vadimtertilov@gmail.com>
2022-05-19 05:09:05 -0700

    Updated to v.1.1 Added simple gui

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
+# Import modules
 import requests
 from bs4 import BeautifulSoup
 import os
 from fake_useragent import UserAgent
+import PySimpleGUI as sg
+import random
 
+sg.theme(random.choice(sg.theme_list()))
 
-site = 'https://wallpapercave.com'
-link = input('Album link - ')
+layout = [[sg.Text('Input your link here')],
+          [sg.Input(key='link')],
+          [sg.Button('Check'), sg.Button('Exit')]]
 
+window = sg.Window('WallPaperCave Manager', layout)
 
-responce = requests.get(link, headers={'User-Agent': UserAgent().chrome}).text
-#open('site.html', 'w', encoding='utf-8').write(responce)
-soup = BeautifulSoup(responce, 'lxml')
-name = soup.find_all('h1')
-folder = str(name[1])[4:-5]
+while True:
+    event, values = window.read()
+    print(event, values)
 
-if not os.path.exists('wallpapers'):
-    os.mkdir('wallpapers')
-if not os.path.exists(f'wallpapers/{folder}'):
-    os.mkdir(f'wallpapers/{folder}')
+    if event in (None, 'Exit', 'Cancel', 'Exit!'):
+        break
 
-images = soup.find_all('div', class_='wallpaper')
+    if event == 'Check':
+        try:
+            responce = requests.get(values['link'], headers={'User-Agent': UserAgent().chrome}).text
+            soup = BeautifulSoup(responce, 'lxml')
+            name = str(soup.find_all('h1')[1])[4:-5]
+            images = soup.find_all('div', class_='wallpaper')
+            count = len(images)
 
-n = 0
-for image in images:
-    n += 1
-    download = image.find('a', class_='download').get('href')
-    final = site + download
-    r = requests.get(final, headers={'User-Agent': UserAgent().chrome}, allow_redirects=True)
-    open(f'wallpapers/{folder}/{n}.jpg', 'wb').write(r.content)
-    print(f'№ {n} is OK')
+            window.extend_layout(window, [[sg.Text(f'The album {name} with {count} wallpapers was discovered.')],
+                                          [sg.Button('Continue'), sg.Button('Cancel')]])
+        except:
+            print('Incorrect link address!! Try again')
+
+    if event == 'Continue':
+        try:
+            if not os.path.exists('wallpapers'):
+                os.mkdir('wallpapers')
+            if not os.path.exists(f'wallpapers/{name}'):
+                os.mkdir(f'wallpapers/{name}')
+
+            window.extend_layout(window, [[sg.Col([[sg.T('Logs')]], scrollable=True, key='logs', s=(200, 200))]])
+
+            n = 0
+            for image in images:
+                n += 1
+                download = image.find('a', class_='download').get('href')
+                final = 'https://wallpapercave.com' + download
+                r = requests.get(final, headers={'User-Agent': UserAgent().chrome}, allow_redirects=True)
+                open(f'wallpapers/{name}/{n}.jpg', 'wb').write(r.content)
+                window.extend_layout(window['logs'], [[sg.Text(f'№{n} is downloaded')]])
+                window.visibility_changed()
+                window['logs'].contents_changed()
+
+            window.extend_layout(window, [[sg.Text('The album was succesfully downloaded!!')], [sg.Button('Exit!')]])
+        except:
+            print('Unknown Error')
+
+window.close()
